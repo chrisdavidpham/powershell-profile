be8a416e7565a36593bab48fcc4e082d7190af73
Chris Pham <chrisdavidpham@gmail.com>
2025-01-16 09:32:02 -0600

    Update profile.ps1
    motivational message

--- profile.ps1
+++ profile.ps1
@@ -1,2 +1,7 @@
-Write-Output "Loading Profile $($MyInvocation.MyCommand.Path)"
-Start-Transcript -OutputDirectory 'c:\logs\powershell'
+Write-Output "Loading $profile`n"
+
+$daysUntilRetimenet = (Get-Date "2055-07-06") - (Get-Date) | Select-Object -ExpandProperty Days
+Write-Output "Days until retirement: $daysUntilRetimenet. One less than yesterday!`n"
+
+$date = (Get-Date).GetDateTimeFormats()[5]
+Start-Transcript -OutputDirectory "c:\logs\powershell\$date" | Out-Null
